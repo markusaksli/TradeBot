255b813dc6763bc79588988785d8e064141844b8
Markus <markusaksli@hotmail.com>
2020-03-17 10:50:46 +0200

    Added binance api and newest lombok

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,14 @@
+import com.webcerebrium.binance.api.BinanceApi;
+import com.webcerebrium.binance.api.BinanceApiException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        try {
+            BinanceApi api = new BinanceApi();
+            System.out.println("ETH-BTC PRICE=" + api.pricesMap().get("ETHBTC"));
+        } catch (
+                BinanceApiException e) {
+            System.out.println("ERROR: " + e.getMessage());
+        }
     }
 }
