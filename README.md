51737eebaee66cac515991f76c0855042aafbe1b
martentyrk <martentyrk@gmail.com>
2020-03-28 17:38:17 +0200

    Fixed ReadMe and got most basic acc info

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # TradeBot
-Crypto trading bot using Binance API
+Crypto trading bot using xChange and Binance API
 
 The bot uses a Binance API key to get live charts and history for the top 5 crypto coins on https://coinmarketcap.com/.
 
-The bot uses 200 MA, 50 MA, RSI and MACD indicators to generate and filter buy signals. The bot automatically places buy limit orders on Binance with a minimum bid, take-profit, and trailing stop-loss. The bot constantly monitors all of its positions for close signals (based on indicators, take-profit or stop-loss).
+The bot uses 200 MA, 50 MA, RSI and MACD indicators to generate and filter buy signals. The bot automatically places buy limit orders on Bitmex with a minimum bid, take-profit, and trailing stop-loss. The bot constantly monitors all of its positions for close signals (based on indicators, take-profit or stop-loss).
 
 The aim of this projext is to create a profitable live trading bot (with paper trading functionality).
--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -22,62 +22,25 @@ import java.net.URI;
 import java.net.http.HttpResponse;
 import org.knowm.xchange.Exchange;
 import org.knowm.xchange.bitmex.dto.account.BitmexAccount;
+import si.mazi.rescu.ClientConfig;
 
 
 public class Account {
     public static void main(String[] args) throws IOException, InterruptedException {
         //Trying to get info with xchange api
-        /*Exchange bitmex = ExchangeFactory.INSTANCE.createExchange(BitmexExchange.class.getName());
+        Exchange bitmex = ExchangeFactory.INSTANCE.createExchange(BitmexExchange.class.getName());
 
         ExchangeSpecification bitmexSpec = bitmex.getExchangeSpecification();
-        bitmexSpec.setHost("testnet.bitmex.com");
         bitmexSpec.setSslUri("https://testnet.bitmex.com");
         bitmexSpec.setApiKey("ZoKlEc3zTsR0L_KLbFQCthKc");
         bitmexSpec.setSecretKey("JCC_KV8_U4T8ZCyEZIceRwzTKTosMj02jcsQqYPGXUB9BkgU");
         bitmex.applySpecification(bitmexSpec);
 
         AccountService accountService = bitmex.getAccountService();
-        System.out.println(accountService.getAccountInfo().getWallet().getBalance(Currency.XBT));*/
+        System.out.println(accountService.getAccountInfo().toString());
 
         //Trying to get info with bitmex api and requests.
         //https://testnet.bitmex.com/api/v1
-        ObjectMapper mapper = new ObjectMapper();
-
-        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-
-        var client = HttpClient.newHttpClient();
-        String API_KEY= "ZoKlEc3zTsR0L_KLbFQCthKc";
-        var request = HttpRequest.newBuilder()
-                .uri(URI.create("https://testnet.bitmex.com/api/v1/user/wallet"))
-                .header("User-Agent", "Mozilla/5.0")
-                .header("x-ratelimit-limit", "60")
-                .header("x-ratelimit-remaining", "58")
-                .header("x-ratelimit-reset", "1489791662")
-                .header("Authorization", new String(API_KEY.getBytes()))
-                .GET()
-                .build();
-
-
-
-        HttpResponse<InputStream> response = client.send(request, HttpResponse.BodyHandlers.ofInputStream());
-        Wallet_Data walletData = mapper.readValue(response.body(), Wallet_Data.class);
-        System.out.println(walletData);
-
-    }
-
-    /**
-     * Is supposed to get info on account.
-     * @throws IOException
-     */
-    public static void AccountInfo() throws IOException {
-        InputStream is =
-                Account.class.getResourceAsStream(
-                        "/org/knowm/xchange/bitmex/dto/account/");
-
-        ObjectMapper mapper = new ObjectMapper();
-        BitmexAccount bitmexAccount = mapper.readValue(is, BitmexAccount.class);
-        System.out.println(bitmexAccount);
-
 
     }
 }
