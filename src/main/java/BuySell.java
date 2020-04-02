931b96baca10e4b97e70ad3987108eeb5e10176b
martentyrk <martentyrk@gmail.com>
2020-04-02 19:09:13 +0300

    Modified Trade class, made currency class, made class for buying and selling, upgraded Acc class

--- src/main/java/BuySell.java
+++ src/main/java/BuySell.java
@@ -1,16 +1,27 @@
-import java.math.BigDecimal;
 import java.sql.Timestamp;
+import java.util.List;
 
 public class BuySell {
+    public void buy(Currency currency, Account account, double amount) {
 
-    //Could also change the parameter to Trade object or smth.
-    public void buy(Currency currency) {
-        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
-        double currentPrice = currency.getPrice();
-        //Trade trade = new Trade(currentPrice, timestamp);
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis()); //Creating timestamp to have the time of trade
+        double currentPrice = currency.getPrice(); //Current price of the currenct
+        //TODO: This trade should have more specifications in the future, right now lets settle with this.
+        Trade trade = new Trade(currentPrice, timestamp, amount);
+        //Adding all necessary to wallet
+        account.addToWallet(currency.getName(), amount);
+        account.addTrade(trade);
 
     }
-    public void sell(Trade currency) {
-
+    public void sell(Currency currency, Account account, double amount) {
+        List<Trade> accountTrades = account.getCurrentTrades();
+        for (Trade trade : accountTrades) {
+            //If trade matches the specifications of a
+            if (trade.getCurrency().getName().equals(currency.getName()) && trade.getAmountOfCurrency() == amount) {
+                account.addTradeHistory(trade);
+                account.removeTrade(trade);
+            }
+        }
+        account.removeFromWallet(currency.getName(), amount);
     }
 }
--- src/main/java/Currency.java
+++ src/main/java/Currency.java
@@ -1,6 +1,24 @@
-import java.math.BigDecimal;
+import com.webcerebrium.binance.api.BinanceApi;
+import com.webcerebrium.binance.api.BinanceApiException;
 
-public interface Currency {
-    public String getName(); //For getting the name of the currency.
-    public double getPrice(); //For getting the current price of the currency.
+public class Currency {
+    private String name;
+
+    public Currency(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() { //Getting the current price using Binance API
+        try {
+            BinanceApi ethereum = new BinanceApi();
+            return (ethereum.pricesMap().get(getName())).doubleValue();
+        } catch(BinanceApiException e) {
+            System.out.println("ERROR: " + e.getMessage());
+            return -1;
+        }
+    }
 }
