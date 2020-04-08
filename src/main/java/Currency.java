bae225689093679efd7b96988d5b760023a9fd59
Markus <markusaksli@hotmail.com>
2020-04-08 18:29:27 +0300

    Removed xchange

--- src/main/java/Currency.java
+++ src/main/java/Currency.java
@@ -1,24 +1,33 @@
 import com.webcerebrium.binance.api.BinanceApi;
 import com.webcerebrium.binance.api.BinanceApiException;
+import com.webcerebrium.binance.datatype.BinanceCandlestick;
+import com.webcerebrium.binance.datatype.BinanceInterval;
+import com.webcerebrium.binance.datatype.BinanceSymbol;
+
+import java.util.List;
 
 public class Currency {
-    private String name;
+    private BinanceSymbol symbol;
 
-    public Currency(String name) {
-        this.name = name;
+    public Currency(String symbol) throws BinanceApiException {
+        this.symbol = BinanceSymbol.valueOf(symbol);
     }
 
     public String getName() {
-        return name;
+        return symbol.getSymbol();
     }
 
     public double getPrice() { //Getting the current price using Binance API
         try {
             BinanceApi ethereum = new BinanceApi();
-            return (ethereum.pricesMap().get(getName())).doubleValue();
-        } catch(BinanceApiException e) {
+            return (ethereum.pricesMap().get(symbol.getSymbol())).doubleValue();
+        } catch (BinanceApiException e) {
             System.out.println("ERROR: " + e.getMessage());
             return -1;
         }
+    }
+
+    public List<BinanceCandlestick> getKlines(int lenght) throws BinanceApiException {
+        return (new BinanceApi()).klines(symbol, BinanceInterval.FIVE_MIN, lenght, null);
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,7 @@
 import com.webcerebrium.binance.api.BinanceApi;
 import com.webcerebrium.binance.api.BinanceApiException;
-import org.knowm.xchange.bitmex.BitmexPrompt;
 
-import java.io.IOException;
-import java.util.concurrent.ThreadLocalRandom;
+
 
 public class Main {
     public static void main(String[] args) {
@@ -16,7 +14,12 @@ public class Main {
             System.out.println("ERROR: " + e.getMessage());
         }
         //Account toomas = new Account("Investor Toomas", 1000);
-        Currency bitcoin = new Currency("BTCUSDT");
+        Currency bitcoin = null;
+        try {
+            bitcoin = new Currency("BTCUSDT");
+        } catch (BinanceApiException e) {
+            e.printStackTrace();
+        }
         System.out.println(bitcoin.getPrice());
     }
 }
