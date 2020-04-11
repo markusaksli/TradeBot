40dfc3fa7a89b05d8361d0712b38a1ee46a1e66a
Markus <markusaksli@hotmail.com>
2020-04-11 17:29:12 +0300

    Converted RSI to exponential averages and double

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,13 @@
-import Indicators.IndicatorFactory;
+import Indicators.RSI;
 import com.webcerebrium.binance.api.BinanceApiException;
 
 public class Main {
     public static void main(String[] args) {
         try {
-            Currency currency = new Currency("XRP");
+            Currency currency = new Currency("LINK");
             System.out.println(currency.getPrice());
+            RSI rsi = new RSI(currency.getCandles(1000), 14);
+            System.out.println(rsi.getTemp(currency.getPrice()));
         } catch (
                 BinanceApiException e) {
             System.out.println("ERROR: " + e.getMessage());
