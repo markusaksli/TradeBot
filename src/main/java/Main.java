fd5a7853f96761bc57c2dc193f6f530d3c12a304
Markus <markusaksli@hotmail.com>
2020-04-18 17:45:09 +0300

    Created singleton Mode class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,34 +1,48 @@
-import com.google.gson.JsonObject;
-import com.webcerebrium.binance.api.BinanceApi;
 import com.webcerebrium.binance.api.BinanceApiException;
-import com.webcerebrium.binance.datatype.BinanceAggregatedTrades;
-import com.webcerebrium.binance.datatype.BinanceCandlestick;
-import com.webcerebrium.binance.datatype.BinanceInterval;
-import com.webcerebrium.binance.datatype.BinanceSymbol;
 import trading.*;
 import trading.Currency;
 import trading.Formatter;
 
-import java.sql.SQLOutput;
 import java.util.*;
 
 public class Main {
     static Set<Currency> currencies; //There should never be two of the same Currency
 
     public static void main(String[] args) throws BinanceApiException {
-        Account toomas = new Account("Investor Toomas", 1000);
-        BuySell.setAccount(toomas);
 
-
-        //Optional for simulation, increases API request limits
         Scanner sc = new Scanner(System.in);
-        System.out.println("---Enter bot mode (live, simulated, backtesting)");
-        String mode = sc.nextLine();
-        if (mode.equals("1")) {
-            System.out.println("Entering BACKTESTING MODE");
+        while (true) {
+            System.out.println("---Enter bot mode (live, simulated, backtesting, collection)");
+            String mode = sc.nextLine();
+            switch (mode) {
+                case "live":
+                    Mode.set(Mode.LIVE);
+                    break;
+                case "simulated":
+                    Mode.set(Mode.SIMULATED);
+                    break;
+                case "backtesting":
+                    Mode.set(Mode.BACKTESTING);
+                    break;
+                case "collection":
+                    Mode.set(Mode.COLLECTION);
+                    break;
+                default:
+                    System.out.println("Incorrect mode");
+                    continue;
+            }
+            break;
+        }
+        System.out.println("---Entering " + Mode.get().name() + " mode");
 
-        } else if (mode.equals("2")) {
-            System.out.println("Entering LIVE mode");
+
+        if (Mode.get() == Mode.BACKTESTING) {
+
+        } else if (Mode.get() == Mode.COLLECTION) {
+
+        } else {
+            Account toomas = new Account("Investor Toomas", 1000);
+            BuySell.setAccount(toomas);
 
             /*while (true) {
                 System.out.println("Enter your API Key: ");
@@ -65,50 +79,50 @@ public class Main {
 
             System.out.println("---SETUP DONE (" + Formatter.formatDecimal(time) + " s)");
 
+            while (true) {
+                System.out.println("Commands: profit, active, history, wallet, currencies");
+                String in = sc.nextLine();
+                switch (in) {
+                    case "profit":
+                        System.out.println("Account profit: " + Formatter.formatPercent(toomas.getProfit()) + "\n");
+                        break;
+                    case "active":
+                        System.out.println("Active trades:");
+                        for (Trade trade : toomas.getActiveTrades()) {
+                            System.out.println(trade);
+                        }
+                        System.out.println(" ");
+                        break;
+                    case "history":
+                        System.out.println("Closed trades:");
+                        for (Trade trade : toomas.getTradeHistory()) {
+                            System.out.println(trade);
+                        }
+                        System.out.println(" ");
+                        break;
+                    case "wallet":
+                        System.out.println("Total wallet value: " + Formatter.formatDecimal(toomas.getTotalValue()) + " USDT");
+                        System.out.println(toomas.getFiat() + " USDT");
+                        for (Map.Entry<Currency, Double> entry : toomas.getWallet().entrySet()) {
+                            if (entry.getValue() != 0) {
+                                System.out.println(entry.getValue() + " " + entry.getKey().getCoin() + " (" + entry.getKey().getPrice() * entry.getValue() + " USDT)");
+                            }
+                        }
+                        System.out.println(" ");
+                        break;
+                    case "currencies":
+                        for (Currency currency : currencies) {
+                            System.out.println(currency);
+                        }
+                        System.out.println(" ");
+                        break;
+                    default:
+                        System.out.println("Wrong input. Try again \n");
+                        break;
+                }
+            }
         }
 
         //From this point we only use the main thread to check how the bot is doing
-        while (true) {
-            System.out.println("Commands: profit, active, history, wallet, currencies");
-            String in = sc.nextLine();
-            switch (in) {
-                case "profit":
-                    System.out.println("Account profit: " + Formatter.formatPercent(toomas.getProfit()) + "\n");
-                    break;
-                case "active":
-                    System.out.println("Active trades:");
-                    for (Trade trade : toomas.getActiveTrades()) {
-                        System.out.println(trade);
-                    }
-                    System.out.println(" ");
-                    break;
-                case "history":
-                    System.out.println("Closed trades:");
-                    for (Trade trade : toomas.getTradeHistory()) {
-                        System.out.println(trade);
-                    }
-                    System.out.println(" ");
-                    break;
-                case "wallet":
-                    System.out.println("Total wallet value: " + Formatter.formatDecimal(toomas.getTotalValue()) + " USDT");
-                    System.out.println(toomas.getFiat() + " USDT");
-                    for (Map.Entry<Currency, Double> entry : toomas.getWallet().entrySet()) {
-                        if (entry.getValue() != 0) {
-                            System.out.println(entry.getValue() + " " + entry.getKey().getCoin() + " (" + entry.getKey().getPrice() * entry.getValue() + " USDT)");
-                        }
-                    }
-                    System.out.println(" ");
-                    break;
-                case "currencies":
-                    for (Currency currency : currencies) {
-                        System.out.println(currency);
-                    }
-                    System.out.println(" ");
-                    break;
-                default:
-                    System.out.println("Wrong input. Try again \n");
-                    break;
-            }
-        }
     }
 }
