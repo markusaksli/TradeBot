88cd84e05ff12fd7a6cd5c869b48df1210d9dc5e
Mahdi Ebrahimi <ebrahimi.mahdi@gmail.com>
2020-11-08 13:06:03 +0330

    Update BuySell.java
    added precision and LOT_SIZE controll

--- src/main/java/trading/BuySell.java
+++ src/main/java/trading/BuySell.java
@@ -4,8 +4,11 @@ package trading;
 import com.binance.api.client.BinanceApiRestClient;
 import com.binance.api.client.domain.account.NewOrderResponse;
 import com.binance.api.client.domain.account.NewOrderResponseType;
+import com.binance.api.client.domain.general.FilterType;
 import com.binance.api.client.exception.BinanceApiException;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.List;
 
 import static com.binance.api.client.domain.account.NewOrder.marketBuy;
@@ -28,9 +31,22 @@ public class BuySell {
     public static void open(Currency currency, String explanation, long timestamp) {
         double currentPrice = currency.getPrice(); //Current price of the currency
         double amount = nextAmount() / currency.getPrice();
+        
+        // round amount
+        int precision = CurrentAPI.get().getExchangeInfo().getSymbolInfo(currency.getPair()).getBaseAssetPrecision();
+        BigDecimal bigDecimal = new BigDecimal(Double.toString(amount));
+        bigDecimal = bigDecimal.setScale(precision, RoundingMode.HALF_DOWN);
+        amount = bigDecimal.doubleValue();
+
         if (amount == 0) {
             System.out.println("---OUT OF FUNDS, CANT OPEN TRADE");
             return; //If no fiat is available, we cant trade
+        }
+        // check LOT_SIZE
+        int minQty = CurrentAPI.get().getExchangeInfo().getSymbolInfo(currency.getPair()).getFilters().stream().filter(f -> FilterType.LOT_SIZE == f.getFilterType()).findFirst().map( f1 -> f1.getMinQty()).map(Integer::parseInt).get();
+        if (amount < minQty) {
+            System.out.println("---OUT OF LOT_SIZE, CANT OPEN TRADE. min LOT_SIZE=" + minQty);
+            return;
         }
         double fiatCost = currentPrice * amount;
         Trade trade = new Trade(currency, currentPrice, amount, explanation);
