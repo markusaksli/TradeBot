279e2075d170ff7857134f863a3da92c81fcaa76
Markus Aksli <markusaksli@hotmail.com>
2021-04-17 16:44:12 +0300

    Minified Main.java

--- src/main/java/system/Main.java
+++ src/main/java/system/Main.java
@@ -1,14 +1,9 @@
 package system;
 
-import trading.*;
-import trading.Currency;
-
 import java.util.*;
 
 
 public class Main {
-    private static List<Currency> currencies;
-
     public static void main(String[] args) {
         //Program config.
         try {
@@ -25,31 +20,6 @@ public class Main {
             new Scanner(System.in).next();
             System.exit(3);
         }
-        System.out.println("Welcome to TradeBot (v0.10.0)\n" +
-                "(made by Markus Aksli, Marten Türk, and Mark Robin Kalder)\n" +
-                "\n" +
-                "This is a cryptocurrency trading bot that uses the Binance API,\n" +
-                "and a strategy based on a couple of 5 minute chart indicators\n" +
-                "(RSI, MACD, Bollinger Bands)\n" +
-                "\n" +
-                "The bot has the following modes of operation:\n" +
-                "---LIVE\n" +
-                "-This mode trades with real money on the Binance platform\n" +
-                "-API key and Secret key required\n" +
-                "---SIMULATION\n" +
-                "-Real-time trading simulation based on actual market data\n" +
-                "-Trades are only simulated based on market prices \n" +
-                "-No actual orders are made\n" +
-                "---BACKTESTING\n" +
-                "-Simulation based on historical data.\n" +
-                "-Allows for quick testing of the behavior and profitability of the bot\n" +
-                "-Data needs to be loaded from a .dat file created with the COLLECTION mode\n" +
-                "---COLLECTION\n" +
-                "-Collects raw market price data from a specified time period\n" +
-                "-Collected data is saved in a file in the /backtesting directory\n" +
-                "-Never run more than one TradeBot with this mode at the same time\n" +
-                "\n" +
-                "Simulation and backtesting do not always reflect live performance\n" +
-                "Make sure you are ready to commit to a strategy before starting LIVE\n");
+        System.out.println("Welcome to TradeBot (v0.11.0)");
     }
 }
